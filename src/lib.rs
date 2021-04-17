27286cd3261ac4c2e63c21b9293ba64ffa9f2f5b
radionnazmiev <radionnazmiev@gmail.com>
2021-04-16 20:45:52 -0700

    Final commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+// fn rgb(r: i32, g: i32, b: i32) -> String {
+//     let validate_range  = |val| match Some(val) {
+//         Some(x) if x > 255 => 255,
+//         Some(x) if x < 0   => 0,
+//           _                => val,
+//     };
+//     format!("{:02x}{:02x}{:02x}",validate_range(r),validate_range(g),validate_range(b)).to_ascii_uppercase()
+// }
+
 fn rgb(r: i32, g: i32, b: i32) -> String {
-    unimplemented!();
+    format!("{0:02X}{1:02X}{2:02X}", r.min(255).max(0), g.min(255).max(0), b.min(255).max(0))
 }
-
-
 
 macro_rules! compare {
   ( $got : expr, $expected : expr ) => {
