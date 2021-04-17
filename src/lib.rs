5d1a18066518b9dd89324be97e431d6105662b49
radionnazmiev <radionnazmiev@gmail.com>
2021-04-16 20:13:24 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+fn rgb(r: i32, g: i32, b: i32) -> String {
+    unimplemented!();
+}
+
+
+
+macro_rules! compare {
+  ( $got : expr, $expected : expr ) => {
+    if $got != $expected { panic!("Got: {}\nExpected: {}\n", $got, $expected); }
+  };
+}
+
+#[cfg(test)]
+mod sample_tests {
+    use self::super::*;
+
+    #[test]
+    fn tests() {
+        compare!(rgb(0, 0, 0), "000000");
+        compare!(rgb(1, 2, 3), "010203");
+        compare!(rgb(255, 255, 255), "FFFFFF");
+        compare!(rgb(254, 253, 252), "FEFDFC");
+        compare!(rgb(-20, 275, 125), "00FF7D");
+    }
+}
